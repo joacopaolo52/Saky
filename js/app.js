860bdf48f63f629db77579355f1850541a4aba5d
Joaquin Paolo <joaquinpaolo1@gmail.com>
2022-04-05 00:26:41 -0300

    Primera Entrega.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+
+/* CONSTRUCTOR DE TIENDA*/
+
 class Tienda {
   constructor (id, nombre, tamaño, precio) {
     this.id = id;
@@ -6,7 +9,9 @@ class Tienda {
     this.precio = precio;
   }
 }
+
         /* PRODUCTOS DE TIENDA*/
+
 const producto0 =  new Tienda(00, "TARTA DE CHOCOLATE", "PORCIÓN", 300);
 const producto1 =  new Tienda(01, "TIRAMISÚ", "PORCIÓN", 300);
 const producto2 =  new Tienda(02, "MUFFIN", "PORCIÓN", 200);
@@ -19,24 +24,42 @@ const producto8 =  new Tienda(08, "CAFÉ", "MEDIANO", 200);
 const producto9 =  new Tienda(09, "MANZANILLA", "MEDIANO", 200);
 const producto10 =  new Tienda(10, "TILO", "MEDIANO", 200);
 const producto11 =  new Tienda(11, "CAPUCCINO", "MEDIANO", 200);
+
         /* ARRAYS DE PRODUCTOS*/
+
 const postres = [producto0, producto1, producto2, producto3, producto4, producto5];
 console.log(postres);
+
 const infusiones = [producto6, producto7, producto8, producto9, producto10, producto11];
 console.log(infusiones)
+
         /* CARRITO*/
+
 const carrito = [];
 const addToCart = (producto) => carrito.push(producto);
-addToCart(producto0);
-addToCart(producto4);
-addToCart(producto10);
+const cantidad = (producto) => {
+    let x = parseInt(prompt(`Elija la cantidad de ${producto.nombre}`));
+    for (let i = 0; i < x; i++) {
+        addToCart(producto);
+    }
+}
+const cuotas =(total) => {
+    let numero = parseInt(prompt(`Elejir cantidad de cuotas`));
+    alert(`El total a pagar es de $${total / numero} en ${numero} cuotas`)
+}
+
+cantidad(producto0);
 console.log(carrito);
+
         /* MAP Y REDUCE PARA CARRITO*/
+
 const precios = carrito.map((producto) => producto.precio)
-console.log(precios);
 const pagar = precios.reduce((acc, item) => acc += item, 0);
 console.log(`El total a pagar es de $${pagar}`);
+cuotas(pagar);
 
+
+        /*SIMULADOR INFUSIONES */
 
 let op;
 let op2;
@@ -58,15 +81,18 @@ while (op != 0) {
             alert(`Elije una opción correcta`)
         }
     if (op2 == 1) {
-        alert(`El precio final es de ${producto6.precio - 50}`);
+        let x = producto6.precio - 50;
+        cuotas(x);
         break;
     }
     else if (op2 == 2) {
-        alert(`El precio final es de ${producto6.precio}`);
+        let x = producto6.precio;
+        cuotas(x);
         break;
     }
     else if (op2 == 3) {
-        alert(`El precio final es de ${producto6.precio + 50}`);
+        let x = producto6.precio + 50;
+        cuotas(x);
         break;
     }
-} 
+}  
