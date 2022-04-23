430bf60217bea6dd29402cd9bfa9871c0e70df9a
Joaquin Paolo <joaquinpaolo1@gmail.com>
2022-04-23 20:46:52 -0300

    segunda entrega y op

--- js/app.js
+++ js/app.js
@@ -1,10 +1,20 @@
 
-    /*Bienvenida */
+    /*  BIENVENIDA E INIT  */
 const init = () => {
-    let bienvenida = document.querySelector("#bienvenidaText");
-    let bienvenidaInput = document.querySelector(`.inputNombre`)
-    bienvenidaInput.addEventListener("change", ()=> bienvenida.innerText = `Hola ${bienvenidaInput.value}, bienvenido a Saky.`)
+    const bienvenida = document.querySelector("#bienvenidaText");
+    const bienvenidaInput = document.querySelector(`.inputNombre`);
+    bienvenidaInput.addEventListener("change", ()=> {
+        localStorage.setItem("NOMBRE", bienvenidaInput.value);
+        bienvenida.innerText = `Hola ${bienvenidaInput.value}, bienvenido a Saky.`;})
+    let nombreGuardado = localStorage.getItem("NOMBRE")
+    JSON.stringify(nombreGuardado);
+/*    if (nombreGuardado == null) {
+    }else {
+        bienvenida.innerText = `Hola ${nombreGuardado}, bienvenido a Saky.`;
+    } */
+    nombreGuardado == null ? nombreGuardado = null : bienvenida.innerText = `Hola ${nombreGuardado}, bienvenido a Saky.`; /* OP TERNARIO */
 }
+
 
 /* CONSTRUCTOR DE TIENDA*/
 
@@ -27,58 +37,12 @@ const producto2 =  new Tienda(02, "MUFFIN", "PORCIÓN", 200);
 const producto3 =  new Tienda(03, "CHOCOTORTA", "PORCIÓN", 270);
 const producto4 =  new Tienda(04, "BROWNIE", "PORCIÓN", 230);
 const producto5 =  new Tienda(05, "LEMON PIE", "PORCIÓN", 300);
-const producto6 =  new Tienda(06, "TÉ NEGRO", "MEDIANO", 200);
-const producto7 =  new Tienda(07, "SUBMARINO", "MEDIANO", 200);
-const producto8 =  new Tienda(08, "CAFÉ", "MEDIANO", 200);
-const producto9 =  new Tienda(09, "MANZANILLA", "MEDIANO", 200);
-const producto10 =  new Tienda(10, "TILO", "MEDIANO", 200);
-const producto11 =  new Tienda(11, "CAPUCCINO", "MEDIANO", 200);
 
-        /* ARRAYS DE PRODUCTOS*/
-
+        /* ARRAY DE PRODUCTOS*/
 const postres = [producto0, producto1, producto2, producto3, producto4, producto5];
 console.log(postres);
 
-const infusiones = [producto6, producto7, producto8, producto9, producto10, producto11];
-console.log(infusiones)
-
-        /* CARRITO*/
-
-const carrito = [];
-const addToCart = (producto) => carrito.push(producto);
-// const cantidad = (producto) => {
-//     let x = parseInt(prompt(`Elija la cantidad de ${producto.nombre}`));
-//     for (let i = 0; i < x; i++) {
-//         addToCart(producto);
-//     }
-// }
-let numero;
-const cuotas = (total) => {
-    while (numero !== 0 || numero !== 3 || numero !== 6 || numero !== 9 || numero !== 12) {
-        numero = parseInt(prompt(`Elejir cantidad de cuotas 3 / 6 / 9 / 12.
-    (Introduzca "0" para un solo pago)`));
-    if (numero == 3 || numero == 6 || numero == 9 || numero == 12 ) {
-        alert(`El total a pagar es de $${total / numero} en ${numero} cuotas`);
-        break;
-    }
-    else if (numero === 0) {
-        alert(`El total a pagar es de $${total / 1} en 1 pago`);
-        break;
-    }
-    }
-}
-
-        /* MAP Y REDUCE PARA CARRITO*/
-
-const precios = carrito.map((producto) => producto.precio)
-const pagar = precios.reduce((acc, item) => acc += item, 0);
-console.log(`El total a pagar es de $${pagar}`);
-// cuotas(pagar);
-
-        /* AGREGAR PRECIOS
-        APP(AGREGAR PRECIOS DE POSTRES[])
-        API(AGREGAR PRECIOS DE INFUSIONES[]) 
-        */
+        /* AGREGAR PRECIOS DE POSTRES[] */
 
 const APP = (postre, x) => {
     let precio = document.createElement("h5");
@@ -104,6 +68,10 @@ for (let i = 1; i < postres.length; i++) {
     APP(postres[i], x);
 }
 
+    /* CARRITO*/
+const carrito = [];
+const addToCart = (producto) => carrito.push(producto);
+
     /*AGREGAR POSTRES AL CARRITO */
 
 const agregarPostre = (x, valor) =>{
@@ -125,3 +93,55 @@ const cantidad = (producto, valor) => {
         addToCart(producto);
     }
 }
+
+    /*ABRIR CARRITO (NO TERMINADO) */
+
+let verCarrito = document.querySelector(`#verCarrito`);
+let listaCarrito = document.querySelector(`#listaCarrito`);
+const imagenCarrito = document.querySelector(`.nav-item-img`);
+const buttonCarrito = document.querySelector("#pagar");
+imagenCarrito.addEventListener('click', () => {
+    verCarrito.setAttribute(`style`, `display: grid;`);
+    verLista();
+});
+
+    /*CERRAR CARRITO */
+const cerrarCarrito = document.querySelector(`#cerrarCarrito`);
+cerrarCarrito.addEventListener('click', () => {
+    verCarrito.setAttribute(`style`, `display: none;`);
+    listaCarrito.innerHTML= `<br>`;
+});
+
+    /*BOTÓN PARA PAGAR. */
+buttonCarrito.addEventListener('click', () => {
+    const precios = carrito.map((producto) => producto.precio);
+    const total = precios.reduce((acc, item) => acc += item, 0);
+    alert(`El total a pagar es: $${total}`);
+    verCarrito.setAttribute(`style`, `display: none;`);
+    listaCarrito.innerHTML= `<br>`;
+    carrito.splice(0,1000); 
+});
+
+    /*CREAR LISTA DE CARRITO */
+const verLista = () => {
+    const lista = document.createElement("ul");
+    listaCarrito.appendChild(lista);
+    let x = 0;
+    carrito.forEach(producto => {
+        let li = document.createElement("li");
+        li.innerHTML = `Producto: ${producto.nombre}, Precio: ${producto.precio}.`;
+        lista.appendChild(li);
+        x++;
+    })
+}
+
+    /*BORRAR ITEM DE TIENDA */
+
+const buttonBorrar = document.querySelector('#borrar');
+buttonBorrar.addEventListener('click', ()=> {
+    carrito.splice(0, 1000);
+    verCarrito.setAttribute(`style`, `display: none;`);
+    listaCarrito.innerHTML= `<br>`;
+})
+
+    /*GUARDAR CARRITO */
